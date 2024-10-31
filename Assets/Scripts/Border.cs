5815c6fc545423fb88d78221f39b55888a6a98e0
preema <7548127+Reety@users.noreply.github.com>
2024-11-01 00:51:41 +1100

    finally got 3/4 of an ugly border around my heading but at what cost

---
+++ Assets/Scripts/Border.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+public class Border : MonoBehaviour
+{
+    [SerializeField] private LineRenderer titleBorder;
+    [SerializeField] private int width;
+
+    [SerializeField] private RectTransform heading;
+    // Start is called before the first frame update
+    
+    
+    void Start()
+    {
+        titleBorder.useWorldSpace = true;
+        Vector3[] titleCorners = new Vector3[4];
+        heading.GetWorldCorners(titleCorners);
+        titleBorder.positionCount = 4;//600;
+
+        Vector3 sign = Vector3.one;
+        int i = 0;
+        
+        foreach (var corner in (titleCorners))
+        {
+            sign.x = (corner.x < heading.position.x) ? -sign.x : sign.x;
+            sign.y = (corner.y < heading.position.y) ? -sign.y : sign.y;
+
+            print($"({corner} is compared to {heading.position} so sign is {sign}");
+            Vector3 newCorner = new Vector3(corner.x + sign.x, corner.y + sign.y);
+            titleBorder.SetPosition(i,newCorner);
+            Debug.Log(newCorner);
+            sign = Vector3.one;
+            i++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
